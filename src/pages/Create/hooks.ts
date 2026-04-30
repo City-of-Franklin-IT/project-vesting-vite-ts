8ec5f1570a25695fb4d396d3e8848abf3984bbeb
Andrew Southern <andrew.southern@franklintn.gov>
2026-04-29 19:41:04 -0500

    Change to redirect navigate path

--- src/pages/Create/hooks.ts
+++ src/pages/Create/hooks.ts
@@ -8,7 +8,7 @@ export const useRedirect = () => {
 
   useEffect(() => {
     if(!isLoading && !isAuthenticated) {
-      navigate('/')
+      navigate('/projects')
     }
   }, [isAuthenticated, isLoading, navigate])
 }
